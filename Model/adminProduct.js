71894b19b9bbcabc231d9f69a5807bb7e657f89b
Vishnurb-7 <vishnurb4vishnu@gmail.com>
2022-10-27 12:43:03 +0530

    last comit

--- Model/adminProduct.js
+++ Model/adminProduct.js
@@ -1,6 +1,7 @@
 const db =require('../config/connection')
 const collection =require('../config/collection');
 const { ObjectId } = require('mongodb');
+const { response } = require('express');
 
 
 module.exports={
@@ -24,5 +25,52 @@ module.exports={
             resolve(response)
           })
         })
+      },
+      showOneProduct:(id)=>{
+        return new Promise ((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT).findOne({_id:ObjectId(id)}).then((response)=>{
+                resolve(response)
+            })
+        })
+      },
+      editProduct:(userid,productDetails)=>{
+        return new Promise (async(resolve,reject)=>{
+            console.log(productDetails)
+            productDetails.Password=await bcrypt.hash(productDetails.Password,10)
+
+            db.get().collection(collection.PRODUCT).updateOne({_id:ObjectId(userid)},{
+                $set:{
+                    
+                }
+            })
+        })
       }
+
 }
+
+editUser:(userid,userDetails)=>{
+    return new Promise(async(resolve,reject)=>{
+        console.log(userDetails)
+        userDetails.Password=await bcrypt.hash(userDetails.Password,10)
+     db.get().collection('user').updateOne({_id:ObjectId(userid)},{
+        $set:{
+            Name:userDetails.Name,
+            Email:userDetails.Email,
+            Password:userDetails.Password
+        }
+     }).then((response)=>{
+        resolve(response)
+     })
+    })
+}
+
+
+
+
+// showOneUser:(id)=>{
+//     return new Promise((resolve,reject)=>{
+//         db.get().collection('user').findOne({_id:ObjectId(id)}).then((response)=>{
+//             resolve(response)
+//         })
+//     })
+// },
